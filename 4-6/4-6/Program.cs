6a695228ecaba6c6920fd09d785e69eba978d3b5
adrienphan <adrienphancaophai@gmail.com>
2021-12-16 12:12:45 +0100

    ajout des derniers exercices

---
+++ 4-6/4-6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_6
+{
+    internal class Program
+    {
+        enum week { 
+        Lundi,
+        Mardi,
+        Mercredi,
+        Jeudi,
+        Vendredi,
+        Samedi,
+        Dimanche
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine((week)i);
+            }
+            Console.WriteLine((week)4);
+            Console.ReadLine();
+        }
+    }
+}
